a6c8ea4230fddcc531fe14545b0c831ccd593c15
Zee <amirabaszarkhah268@gmail.com>
2026-02-16 13:22:29 +0330

    Add all project files

---
+++ shop/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Product
+# Create your views here.
+def index_view(request):
+    context = {
+        'popular_games':Product.objects.all()
+    }
+    return render(request,"index.html")
